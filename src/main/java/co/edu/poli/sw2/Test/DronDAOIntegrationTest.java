1fc7c434506e4f8ed97edb9bda19b608b7935a16
Miilo00 <deapoolomg@gmail.com>
2026-08-19 12:35:57 -0500

    Update DronDAOIntegrationTest.java
    Correcion

--- src/main/java/co/edu/poli/sw2/Test/DronDAOIntegrationTest.java
+++ src/main/java/co/edu/poli/sw2/Test/DronDAOIntegrationTest.java
@@ -3,6 +3,7 @@ package co.edu.poli.sw2.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ import co.edu.poli.sw2.Dao.ConexionBD;
  * base de datos.
  *
  * <p>
- * Esta clase comprueba que el método {@link ConexionBD#conectar()} pueda
+ * Esta clase comprueba que el método {@link ConexionBD#getInstance()} pueda
  * establecer correctamente una conexión con la base de datos.
  * </p>
  *
@@ -28,11 +29,12 @@ class DronDAOIntegrationTest {
 	 * <p>
 	 * La prueba falla si el método de conexión retorna {@code null}.
 	 * </p>
+	 * @throws SQLException 
 	 */
 	@Test
-	void probarConexionBD() {
+	void probarConexionBD() throws SQLException {
 
-		Connection conexion = ConexionBD.conectar();
+		Connection conexion = ConexionBD.getInstance().getConexion();
 
 		assertNotNull(conexion);
 	}
