ea763b0407ff3ecbeb6131106da28192e9bbfe38
Miilo00 <deapoolomg@gmail.com>
2026-08-25 18:06:03 -0500

    Update DronFactory.java

--- src/main/java/co/edu/poli/sw2/Service/DronFactory.java
+++ src/main/java/co/edu/poli/sw2/Service/DronFactory.java
@@ -1,5 +1,22 @@
 package co.edu.poli.sw2.Service;
 
-public class DronFactory {
+import co.edu.poli.sw2.model.Dron;
+
+/**
+ * Interfaz que define la fábrica abstracta para la creación de objetos de tipo {@link Dron}.
+ * Forma parte de la implementación del patrón de diseño Factory Method.
+ * 
+ * @author Jeisson
+ * @author Camilo
+ * @version 1.0
+ */
+public interface DronFactory {
+
+    /**
+     * Crea y retorna una nueva instancia concreta de la clase {@link Dron}.
+     * 
+     * @return Objeto instanciado de tipo {@link Dron}.
+     */
+    Dron crearDron();
 
 }
